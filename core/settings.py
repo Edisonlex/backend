6c36233e37ca5acd5b7a08fbac0977a47ad4a446
Edisonlex <148796409+Edisonlex@users.noreply.github.com>
2025-01-31 18:22:22 -0500

    aaa

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [".vercel.app",'127.0.0.1']
 # Application definition
 
 DJANGO_APPS = [
+    'unfold',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
