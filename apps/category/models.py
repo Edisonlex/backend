b3aab41c099aa43050ecd3fa207af750db10dad0
Edisonlex <148796409+Edisonlex@users.noreply.github.com>
2025-01-31 22:23:08 -0500

    aaa

--- apps/category/models.py
+++ apps/category/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
@@ -10,12 +9,12 @@ class Category(models.Model):
         'self', related_name='children', on_delete=models.CASCADE, blank=True, null=True)
     
     name = models.CharField(max_length=255, unique=True)
-    thumbnail =         models.ImageField(upload_to='media/categories/')
+    thumbnail = models.URLField(blank=True, null=True)  
 
     def __str__(self):
         return self.name
 
     def get_thumbnail(self):
         if self.thumbnail:
-            return self.thumbnail.url
+            return self.thumbnail
         return ''
